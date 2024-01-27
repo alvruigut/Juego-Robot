b22be52da3d2be80949b043b3af9ab2ae46c5ced
ALVARO RUIZ <alvruigut@alum.us.es>
2024-01-27 16:44:40 +0100

    quitar cosas y traje buzo

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from model.Inventario import *
 #Volumen
 foto_volumen = pygame.image.load("assest/volumen.png")
 volumen=pygame.transform.scale(foto_volumen, (50,50))
-
 #0 posicion inicial,1 back,2 left,3 right
 sprite_steven=[pygame.image.load("assest/steve_front.png"),pygame.image.load("assest/steve_back.png"),pygame.image.load("assest/steve_left.png"),pygame.image.load("assest/steve_right.png")]
 sprite_enderman=[pygame.image.load("assest/enderman_front.png"),pygame.image.load("assest/enderman_back.png"),pygame.image.load("assest/enderman_x.png")]
@@ -21,30 +20,49 @@ sprite_enderman=[pygame.image.load("assest/enderman_front.png"),pygame.image.loa
 tam_x_ventana = 1900
 tam_y_ventana = 980
 ventana = pygame.display.set_mode((tam_x_ventana, tam_y_ventana))
+foto_fondo = pygame.image.load("assest/fondo.jpg")
+fondo=pygame.transform.scale(foto_fondo, (tam_x_ventana,tam_y_ventana))
 #Steven
 steven=Steve(0,0)
-posicion_x_steven = random.randint(560, tam_x_ventana-steven.body.width)
+posicion_x_steven = random.randint(600, tam_x_ventana-steven.body.width)
 posicion_y_steven= random.randint(0, tam_y_ventana-steven.body.height)
 steven.body.x = posicion_x_steven
 steven.body.y = posicion_y_steven
-#Lava
-lava=Lava(0,0)
-#ManzanaOro
-manzana_oro=ManzanaDorada(0,tam_y_ventana/2-lava.body.height/2-5)
-#Manzana
-manzana=Manzana(0,900)
 #Agua
-agua=Agua(540,325)
+agua=Agua(540,327)
+#Lava
+lavas=[]
+lava=Lava(0,0)
+for x in range(6):
+    for y in range(2):
+        lava=Lava(x*lava.body.width,y*(lava.body.height+steven.body.height+10)+tam_y_ventana/3) 
+        lavas.append(lava)  
 
-#Objetos
+#ManzanaOro
+manzanas_oro=[]
+manzana_oro=ManzanaDorada(0,tam_y_ventana/2-lava.body.height/2-5)
+manzanas_oro.append(manzana_oro)
+#Manzana
+manzanas=[]
+manzana=Manzana(0,900)
+manzana2=Manzana(0,700)
+manzanas.append(manzana)
+manzanas.append(manzana2)
+
+#Diamantes
+diamantes=[]
 diamante=Diamante(700,700)
-
+diamantes.append(diamante)
+#Perla
+perlas=[]
 perla=Perla(0,60)
-
+perlas.append(perla)
+#TNT
+tnts=[]
 tnt=Tnt(900,30)
+tnts.append(tnt)
 
-foto_fondo = pygame.image.load("assest/fondo.jpg")
-fondo=pygame.transform.scale(foto_fondo, (tam_x_ventana,tam_y_ventana))
+
 
 
 pygame.init()
@@ -63,64 +81,75 @@ while game_running:
     diamantes_texto = fuente.render(f'Diamantes: {steven.diamantes}', True, (0, 0, 0))
     inventario_texto = fuente.render(f'Inventario: {steven.inventario.objetos}', True, (0, 0, 0))
 
-    tnt_texto = fuente.render(f'Tnt: {steven.tnt}', True, (0, 0, 0))
     ventana.blit(fondo, (0, 0))
     ventana.blit(volumen, (tam_x_ventana-50, 0))
-    ventana.blit(manzana_oro.image, manzana_oro.body)
-    ventana.blit(manzana.image, manzana.body)
-    ventana.blit(tnt.image, tnt.body)
+
+    for manzana_oro in manzanas_oro:
+        steven.comer_manzana_dorada(manzana_oro,manzanas_oro)
+        ventana.blit(manzana_oro.image, manzana_oro.body)
+    for manzana in manzanas:
+        steven.comer_manzana(manzana,manzanas)
+        ventana.blit(manzana.image, manzana.body)
+        
+
     ventana.blit(vida_texto, (10, 10))
     ventana.blit(inventario_texto, (900, 10))
+    for tnt in tnts:
+        ventana.blit(tnt.image, tnt.body)
+        steven.coger_tnt(tnt,tnts)
+
+    for diamante in diamantes:
+        ventana.blit(diamante.image, diamante.body)
+        steven.coger_diamante(diamante,diamantes)
+    for perla in perlas:
+        ventana.blit(perla.image, perla.body)
+        steven.convertir_enderman(perla,perlas)
+    steven.cambiar_enderman()
+
+    for lava in lavas:
+        ventana.blit(lava.image, lava.body)
+        steven.collision_lava(lava.body)
     ventana.blit(agua.image, agua.body)
     ventana.blit(steven.image, steven.body)
-    ventana.blit(perla.image, perla.body)
-    ventana.blit(diamante.image, diamante.body)
 
-
-
-
-
-    steven.convertir_enderman(perla.body)
-    steven.comer_manzana_dorada(manzana_oro.body)
-    steven.comer_manzana(manzana.body)
-    steven.coger_diamante(diamante.body)
-    steven.coger_tnt(tnt.body)
-
-    for x in range(6):
-        for y in range(2):
-            lava=Lava(x*lava.body.width,y*(lava.body.height+steven.body.height+10)+tam_y_ventana/3)    
-            ventana.blit(lava.image, lava.body)
-                
     buttom_pressed = pygame.key.get_pressed()
     if buttom_pressed[K_LEFT]:
         if steven.enderman==False:
-            steven.move(-steven.velocidad,0,tam_x_ventana,tam_y_ventana,agua.body,lava.body)  # mover a la izquierda
+            steven.move(-steven.velocidad,0,tam_x_ventana,tam_y_ventana,agua.body)  # mover a la izquierda
             steven.image=pygame.transform.scale(sprite_steven[2], (steven.gorditura_steven-14, steven.altura_steven-3))
         else:
-            steven.move(-steven.velocidad,0,tam_x_ventana,tam_y_ventana,agua.body,lava.body)  # mover a la izquierda
+            steven.move(-steven.velocidad,0,tam_x_ventana,tam_y_ventana,agua.body)  # mover a la izquierda
             steven.image=pygame.transform.scale(sprite_enderman[2], (steven.gorditura_steven-14, steven.altura_steven-3))
     if buttom_pressed[K_RIGHT]:
         if steven.enderman==False:
-            steven.move(steven.velocidad, 0,tam_x_ventana,tam_y_ventana, agua.body,lava.body)  # mover a la derecha
+            steven.move(steven.velocidad, 0,tam_x_ventana,tam_y_ventana, agua.body)  # mover a la derecha
             steven.image=pygame.transform.scale(sprite_steven[3], (steven.gorditura_steven-10, steven.altura_steven-5))
         else:            
-            steven.move(steven.velocidad, 0,tam_x_ventana,tam_y_ventana, agua.body,lava.body)  # mover a la derecha
+            steven.move(steven.velocidad, 0,tam_x_ventana,tam_y_ventana, agua.body)  # mover a la derecha
             steven.image=pygame.transform.scale(sprite_enderman[2], (steven.gorditura_steven-10, steven.altura_steven-5))
 
     if buttom_pressed[K_UP]:
         if steven.enderman==False:
-            steven.move(0, -steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body,lava.body)  # mover hacia arriba
+            steven.move(0, -steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body)  # mover hacia arriba
             steven.image=pygame.transform.scale(sprite_steven[1], (steven.gorditura_steven, steven.altura_steven))
         else:
-            steven.move(0, -steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body,lava.body)  # mover hacia arriba
+            steven.move(0, -steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body)  # mover hacia arriba
             steven.image=pygame.transform.scale(sprite_enderman[1], (steven.gorditura_steven, steven.altura_steven))
     if buttom_pressed[K_DOWN]:
         if steven.enderman==False:
-            steven.move(0, steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body,lava.body)  # mover hacia abajo
+            steven.move(0, steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body)  # mover hacia abajo
             steven.image=pygame.transform.scale(sprite_steven[0], (steven.gorditura_steven, steven.altura_steven))
         else:
-            steven.move(0, steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body,lava.body)  # mover hacia abajo
+            steven.move(0, steven.velocidad,tam_x_ventana,tam_y_ventana,agua.body)  # mover hacia abajo
             steven.image=pygame.transform.scale(sprite_enderman[0], (steven.gorditura_steven, steven.altura_steven))
+
+
+
+  
+    
+
+                
+   
 
             
    
--- model/Steven.py
+++ model/Steven.py
@@ -26,25 +26,30 @@ class Steve(pygame.sprite.Sprite):
         self.estado_x = x
         self.estado_y = y
 
-    def move(self, dx, dy,ventana_horizontal,ventana_vertical, agua,lava):
+    def move(self, dx, dy,ventana_horizontal,ventana_vertical, agua):
         x = self.body.x
         y = self.body.y
         if 0 <= self.body.x + dx < ventana_horizontal - self.body.width:
             self.body.x += dx
         if 0 <= self.body.y + dy< ventana_vertical - self.body.height:
             self.body.y += dy
-        if self.collision_agua(agua) or self.collision_lava(lava):
+        if self.collision_agua(agua):
             self.body.x = x
             self.body.y = y
             self.vida -= 1
             time.sleep(0.2)
-        if not 0 <= self.body.x < ventana_horizontal - self.body.width or not 0 <= self.body.y < ventana_vertical - self.body.height:
-            self.vida -= 1
-            time.sleep(0.3)
+
 
 
     def collision_lava(self,lava):
-        return self.body.colliderect(lava)
+        x = self.body.x
+        y = self.body.y
+        if self.body.colliderect(lava):
+            self.body.x = x
+            self.body.y = y
+            self.vida -= 1
+            time.sleep(0.4)
+
   
     
     def collision_agua(self,agua):
@@ -53,50 +58,71 @@ class Steve(pygame.sprite.Sprite):
         else:
             return False        
         
-    def convertir_enderman(self,perla):
+    def convertir_enderman(self,perla,perlas):
         tecla = pygame.key.get_pressed()
-        if  tecla[K_t] and self.body.colliderect(perla) and self.enderman==False:
+        if  tecla[K_t] and self.body.colliderect(perla.body) and self.enderman==False:
             self.perla=1
             self.enderman=True
             self.vida+=5
+            self.inventario.objetos['Perla']=self.perla
             foto_enderman=pygame.image.load("assest/enderman_front.png")
             enderman=pygame.transform.scale(foto_enderman, (self.gorditura_steven,self.altura_steven))
             self.image=enderman
+            perlas.remove(perla)
             time.sleep(0.4)
-        elif tecla[K_t] and self.body.colliderect(perla) and self.enderman==True:
+
+    def cambiar_enderman(self):
+        tecla = pygame.key.get_pressed()
+        if tecla[K_p] and self.perla==1 and self.enderman==True:
             self.enderman=False
             foto_steven=pygame.image.load("assest/steve_front.png")
             steve=pygame.transform.scale(foto_steven, (self.gorditura_steven,self.altura_steven))
             self.image=steve
             time.sleep(0.4)
-            
-    def comer_manzana_dorada(self,manzana):
+        elif tecla[K_p] and self.perla==1 and self.enderman==False:
+            self.enderman=True
+            foto_enderman=pygame.image.load("assest/enderman_front.png")
+            enderman=pygame.transform.scale(foto_enderman, (self.gorditura_steven,self.altura_steven))
+            self.image=enderman
+            time.sleep(0.4)
+
+    def comer_manzana_dorada(self,manzana,manzanas):
         tecla = pygame.key.get_pressed()
-        if tecla[K_t] and self.body.colliderect(manzana):
+        if tecla[K_t] and self.body.colliderect(manzana.body):
             if self.enderman==False: 
                 self.vida = 10
+                manzanas.remove(manzana)
             else:
                 self.vida = 15
+                manzanas.remove(manzana)
 
-    def comer_manzana(self,manzana):
+    def comer_manzana(self,manzana,manzanas):
         tecla = pygame.key.get_pressed()
-        if tecla[K_t] and self.body.colliderect(manzana):
-            if self.vida<10:
+        if tecla[K_t] and self.body.colliderect(manzana.body):
+            if self.vida<10 and self.enderman==False:
+                manzanas.remove(manzana)
+                self.vida += 1
+                time.sleep(0.3)
+            elif self.vida<15 and self.enderman==True:
+                manzanas.remove(manzana)
                 self.vida += 1
                 time.sleep(0.3)
 
-    def coger_tnt(self,tnt):
+
+    def coger_tnt(self,tnt,tnts):
         tecla = pygame.key.get_pressed()
-        if tecla[K_t] and self.body.colliderect(tnt):
+        if tecla[K_t] and self.body.colliderect(tnt.body):
             self.tnt += 1
+            tnts.remove(tnt)
             self.inventario.objetos['TNT']=self.tnt
             time.sleep(0.3)
 
-    def coger_diamante(self,diamante):
+    def coger_diamante(self,diamante,diamantes):
         tecla = pygame.key.get_pressed()
-        if tecla[K_t] and self.body.colliderect(diamante):
+        if tecla[K_t] and self.body.colliderect(diamante.body):
             self.diamantes += 1
             self.inventario.objetos['Diamante']=self.diamantes
+            diamantes.remove(diamante)  
             time.sleep(0.3)
 
     
